f6ca97f3697f3b2431bc86acaf39afb605260838
Brian Cline <standardhuman@gmail.com>
2017-01-05 09:52:34 -0800

    provision db

--- src/app.js
+++ src/app.js
@@ -1,19 +1,6 @@
 const Server = require('./server.js')
 const port = (process.env.PORT || 8080)
 const app = Server.app()
-const pg = require('pg');
-
-pg.defaults.ssl = false;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
 
 if (process.env.NODE_ENV !== 'production') {
   const webpack = require('webpack')
--- src/index.js
+++ src/index.js
@@ -16,16 +16,18 @@ app.get('/db', function (request, response) {
     });
   });
 });
-// pg.defaults.ssl = true;
-// pg.connect(process.env.DATABASE_URL, function(err, client) {
-//   if (err) throw err;
-//   console.log('Connected to postgres! Getting schemas...');
-//
-//   client
-//     .query('SELECT table_schema,table_name FROM information_schema.tables;')
-//     .on('row', function(row) {
-//       console.log(JSON.stringify(row));
-//     });
-// });
+
+pg.defaults.ssl = true;
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 render(<App/>, document.getElementById('main'))
