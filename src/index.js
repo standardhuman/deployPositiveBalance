1f573b7143f59f1c09f0427523c15d1fd50b42c3
Brian Cline <standardhuman@gmail.com>
2017-01-04 16:27:49 -0800

    update DATABASE_URL

--- src/index.js
+++ src/index.js
@@ -3,4 +3,29 @@ import { render } from 'react-dom'
 import App from './components/app'
 import './styles/app.scss'
 
+const pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+// pg.defaults.ssl = true;
+// pg.connect(process.env.DATABASE_URL, function(err, client) {
+//   if (err) throw err;
+//   console.log('Connected to postgres! Getting schemas...');
+//
+//   client
+//     .query('SELECT table_schema,table_name FROM information_schema.tables;')
+//     .on('row', function(row) {
+//       console.log(JSON.stringify(row));
+//     });
+// });
+
 render(<App/>, document.getElementById('main'))
